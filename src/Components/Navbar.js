06225abcb1e074f7757a3cf7e6c8ab8ea5e7e020
SurajiEkanayaka <surajiekanayaka4@gmail.com>
2021-10-14 10:55:30 +0530

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import {
 MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
-MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem
+MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem,MDBFooter, MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText,    
 } from "mdbreact";
 import { BrowserRouter as Router } from 'react-router-dom';
+import "./navbar.css";
 
 class NavbarPage extends Component {
 state = {
@@ -58,7 +59,61 @@ render() {
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBNavbar>
+      
+      <MDBCol>
+      <MDBCard style={{ width: "22rem"}} className="card1">
+        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make
+            up the bulk of the card&apos;s content.
+          </MDBCardText>
+          <MDBBtn href="#">MDBBtn</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+
+
+
+
+
+
+      <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
+      <MDBContainer className='p-4'>
+        <MDBRow>
+          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Footer text</h5>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
+              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
+              est atque cumque eum delectus sint!
+            </p>
+          </MDBCol>
+
+          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
+            <h5 className='text-uppercase'>Footer text</h5>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
+              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam,
+              est atque cumque eum delectus sint!
+            </p>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        &copy; {new Date().getFullYear()} Copyright:{' '}
+        <a className='text-dark' href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
+     
     </Router>
+    
     );
   }
 }
