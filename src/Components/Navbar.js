b8e0fad34d3075b471bb73200e4ef0edb9d77586
SurajiEkanayaka <surajiekanayaka4@gmail.com>
2021-10-14 11:04:53 +0530

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -73,6 +73,20 @@ render() {
         </MDBCardBody>
       </MDBCard>
     </MDBCol>
+    <MDBCol>
+      <MDBCard style={{ width: "22rem"}} className="card1">
+        <MDBCardImage className="img-fluid" src="https://mdbootstrap.com/img/Photos/Others/images/43.jpg" waves />
+        <MDBCardBody>
+          <MDBCardTitle>Card title</MDBCardTitle>
+          <MDBCardText>
+            Some quick example text to build on the card title and make
+            up the bulk of the card&apos;s content.
+          </MDBCardText>
+          <MDBBtn href="#">MDBBtn</MDBBtn>
+        </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+    
 
 
 
